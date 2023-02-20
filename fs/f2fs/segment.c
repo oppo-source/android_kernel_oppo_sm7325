99028e48aac51ed65c573b48f6192326e830a90d
pswbuild <pswbuild@oppo.com>
2023-02-21 01:01:14 +0800

    Synchronize codes for OPPO Reno7 ColorOS 13.0.0

--- fs/f2fs/segment.c
+++ fs/f2fs/segment.c
@@ -3048,7 +3048,6 @@ static int __get_segment_type_6(struct f2fs_io_info *fio)
 {
 	if (fio->type == DATA) {
 		struct inode *inode = fio->page->mapping->host;
-
 		if (is_cold_data(fio->page) || file_is_cold(inode) ||
 				f2fs_compressed_file(inode))
 			return CURSEG_COLD_DATA;
@@ -3250,7 +3249,11 @@ void f2fs_do_write_meta_page(struct f2fs_sb_info *sbi, struct page *page,
 		.type = META,
 		.temp = HOT,
 		.op = REQ_OP_WRITE,
+#if defined(OPLUS_FEATURE_UFSPLUS) && defined(CONFIG_FS_HPB)
+		.op_flags = REQ_SYNC | REQ_META | REQ_PRIO | REQ_HPB_PREFER,
+#else
 		.op_flags = REQ_SYNC | REQ_META | REQ_PRIO,
+#endif
 		.old_blkaddr = page->index,
 		.new_blkaddr = page->index,
 		.page = page,
@@ -3311,6 +3314,10 @@ int f2fs_inplace_write_data(struct f2fs_io_info *fio)
 			  __func__, segno);
 		return -EFSCORRUPTED;
 	}
+
+	if (fio->post_read)
+		invalidate_mapping_pages(META_MAPPING(sbi),
+					fio->new_blkaddr, fio->new_blkaddr);
 
 	stat_inc_inplace_blocks(fio->sbi);
 
@@ -3472,10 +3479,16 @@ void f2fs_wait_on_block_writeback(struct inode *inode, block_t blkaddr)
 void f2fs_wait_on_block_writeback_range(struct inode *inode, block_t blkaddr,
 								block_t len)
 {
+	struct f2fs_sb_info *sbi = F2FS_I_SB(inode);
 	block_t i;
+
+	if (!f2fs_post_read_required(inode))
+		return;
 
 	for (i = 0; i < len; i++)
 		f2fs_wait_on_block_writeback(inode, blkaddr + i);
+
+	invalidate_mapping_pages(META_MAPPING(sbi), blkaddr, blkaddr + len - 1);
 }
 
 static int read_compacted_summaries(struct f2fs_sb_info *sbi)
